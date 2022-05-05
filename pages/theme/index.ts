b955e5234a246c775980bdf51e8ffd0576a22fe2
Cristian Ojeda <cristianojeda.maimo@gmail.com>
2022-05-05 12:49:37 -0300

    use theme extensions: withDefaultColorScheme and withDefaultVariant

--- pages/theme/index.ts
+++ pages/theme/index.ts
@@ -1,4 +1,4 @@
-import { extendTheme, theme as themeBase } from "@chakra-ui/react";
+import { extendTheme, theme as themeBase, withDefaultColorScheme, withDefaultVariant } from "@chakra-ui/react";
 
 
 
@@ -21,6 +21,15 @@ const theme = extendTheme({
             900: '#203300',
         }
     }
-})
+},
+    withDefaultColorScheme({
+        colorScheme: 'brand',
+        components: ['Checkbox']
+    }),
+    withDefaultVariant({
+        variant: 'filled',
+        components:['Input', 'Select']
+    }),
+)
 
 export default theme;
