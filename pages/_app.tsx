c25ff67def2b1551f711a1d637477b2320222075
Cristian Ojeda <cristianojeda.maimo@gmail.com>
2022-05-05 12:00:07 -0300

    custom colors and fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 import { Component } from 'react';
+import theme from './theme';
+import './theme/styles.css'
 
 const App = ({ Component, pageProps }: AppProps) => {
     return (
-        <ChakraProvider>
+        <ChakraProvider theme={ theme }>
             <Component {...pageProps} />
         </ChakraProvider>
     )
